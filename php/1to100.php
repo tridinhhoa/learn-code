808bbe36ace8d7d74f9ee66c3a46c1750fc4d71b
tridinhhoa <tridinhhoa@gmail.com>
2021-11-29 12:19:00 +0700

    php-j2teamschool
    Exercise of webcode

---
+++ php/1to100.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h1>
+	 In ra dãy số từ 1 tới 100
+	</h1>
+	<?php
+		for ($a = 1; $a <= 100; $a++) {
+			echo("$a ");
+		}
+	?>
+</body>
+</html>
